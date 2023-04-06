137acb0332f714516255caabe5056402cc2681ab
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-04-06 21:42:13 +0900

    리팩토링 : DELETE 메소드 방식으로 변경 - hiddenmethod 설정 추가

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -13,10 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -67,7 +64,7 @@ public class LikeablePersonController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public String remove(@PathVariable("id") Long id) {
         RsData<LikeablePerson> rsData = likeablePersonService.deleteById(rq.getMember(), id);
 
