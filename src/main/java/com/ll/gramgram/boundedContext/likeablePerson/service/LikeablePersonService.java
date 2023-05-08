0735d812d5af2e4d371f524f3b825453f2d9e291
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-05-08 15:25:40 +0900

    [feat] 내가 받은 호감리스트 성별 필터링 1차 구현

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -15,9 +15,11 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -218,5 +220,21 @@ public class LikeablePersonService {
 
 
         return RsData.of("S-1", "호감사유변경이 가능합니다.");
+    }
+
+    public List<LikeablePerson> listing(InstaMember instaMember, String gender, String attractiveTypeCode, String sortCode) {
+        List<LikeablePerson> likeablePeople = instaMember.getToLikeablePeople();
+
+        System.out.println("gender = " + gender);
+        System.out.println("attractiveTypeCode = " + attractiveTypeCode);
+        System.out.println("sortCode = " + sortCode);
+
+        if(gender == null || gender.isBlank()) {
+            return likeablePeople;
+        } else {
+            return likeablePeople.stream()
+                    .filter(e -> e.getFromInstaMember().getGender().equals(gender))
+                    .collect(Collectors.toList());
+        }
     }
 }
